88d10839c8d5bd76279692c6f2eee52da5ffaedf
Dalal Alsabah <dasabah@gmail.com>
2022-10-31 19:51:37 +0300

    first task completed

--- airport/settings.py
+++ airport/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "flights",
-]
+    "rest_framework",]
+    
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
--- flights/views.py
+++ flights/views.py
@@ -1,1 +1,16 @@
+from rest_framework.generics import ListAPIView
 from .models import Booking, Flight
+from .serializers import FlightListSerializer, BookingListSerializer
+from .models  import Booking
+from django.utils import timezone
+
+class FlightListView(ListAPIView):
+    queryset = Flight.objects.all()
+    serializer_class= FlightListSerializer
+
+class BookingListView(ListAPIView):
+    queryset = Booking.objects.filter(date__gt=timezone.now())
+    serializer_class = BookingListSerializer
+
+    
+    
